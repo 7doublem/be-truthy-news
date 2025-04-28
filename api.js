67f4ebe3d320921f15e4d2c6ccc406bf8f49afbc
MM <mm@Mac.mynet>
2025-04-28 16:44:29 +0100

    add getAllTopics, 404 and 500 error handlers

--- api.js
+++ api.js
@@ -1,10 +1,25 @@
 const express = require("express");
 const app = express();
 const db = require("./db/connection");
-const { getApi } = require("./app/controller/controller");
+const { getApi, getAllTopics } = require("./app/controller/controller");
 
 app.use(express.json());
 
+// get api
 app.get("/api", getApi);
+
+// get all topics
+app.get("/api/topics", getAllTopics);
+
+// 404 handler
+app.all("/*splat", (req, res) => {
+  res.status(404).send({ msg: "Not Found" });
+});
+
+// 500 handler
+app.use((err, req, res, next) => {
+  console.log(err);
+  res.status(500).send({ msg: "Internal Server Error" });
+});
 
 module.exports = app;
