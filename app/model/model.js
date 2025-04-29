a0a92412e077094bfd780d9fd1a39d8c79ccfddc
MM <mm@Mac.mynet>
2025-04-29 15:16:52 +0100

    add selectArticlesById model

--- app/model/model.js
+++ app/model/model.js
@@ -1,10 +1,28 @@
 const db = require("../../db/connection");
 
 const selectAllTopics = () => {
-  return db.query(`SELECT * FROM topics`).then((result) => {
-    const topics = result.rows;
-    return topics;
+  return db.query(`SELECT * FROM topics`).then(({ rows }) => {
+    return rows;
   });
 };
 
-module.exports = { selectAllTopics };
+const selectArticlesById = (article_id) => {
+  return db
+    .query(
+      `SELECT * FROM articles 
+        WHERE article_id = $1::int`,
+      [article_id]
+    )
+    .then(({ rows }) => {
+      if (rows.length === 0) {
+        return Promise.reject({
+          status: 404,
+          msg: `No article found for article_id: ${article_id}`,
+        });
+      }
+      const article = rows[0];
+      return article;
+    });
+};
+
+module.exports = { selectAllTopics, selectArticlesById };
