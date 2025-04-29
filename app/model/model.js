05c8b8231d122759229b507c4f3fe03283976b85
7doublem <76698783+7doublem@users.noreply.github.com>
2025-04-29 20:26:47 +0100

    add selectAlLArticles model

--- app/model/model.js
+++ app/model/model.js
@@ -25,4 +25,18 @@ const selectArticlesById = (article_id) => {
     });
 };
 
-module.exports = { selectAllTopics, selectArticlesById };
+const selectAllArticles = () => {
+  return db
+    .query(
+      `SELECT articles.author, articles.title, articles.article_id, articles.topic, articles.created_at, articles.votes, articles.article_img_url, COUNT(comments.article_id)::int AS comment_count
+        FROM articles
+        LEFT JOIN comments
+        ON articles.article_id = comments.article_id
+        GROUP BY articles.article_id
+        ORDER BY created_at DESC`
+    )
+    .then(({ rows }) => {
+      return rows;
+    });
+};
+module.exports = { selectAllTopics, selectArticlesById, selectAllArticles };
