ef10ea59abe029e2b848c13b36fc491ab3394224
7doublem <76698783+7doublem@users.noreply.github.com>
2025-04-29 20:24:54 +0100

    remove console logs

--- db/queries.js
+++ db/queries.js
@@ -15,25 +15,25 @@ db.query(`SELECT * FROM users`)
         // Get all of the comments where the votes are less than zero
         db.query(`SELECT * FROM comments WHERE votes < 0`)
           .then((result) => {
-            console.log(result.rows);
+            //console.log(result.rows);
           })
           .then(() => {
             // Get all of the topics
             db.query(`SELECT * FROM topics`)
               .then((result) => {
-                console.log(result.rows);
+                //console.log(result.rows);
               })
               .then(() => {
                 // Get all of the articles by user grumpy19
                 db.query(`SELECT * FROM articles WHERE author = 'grumpy19';`)
                   .then((result) => {
-                    console.log(result.rows);
+                    //console.log(result.rows);
                   })
                   .then(() => {
                     // Get all of the comments that have more than 10 votes
                     db.query(`SELECT * FROM comments where votes > 10`).then(
                       (result) => {
-                        console.log(result.rows);
+                        //console.log(result.rows);
                       }
                     );
                   });
