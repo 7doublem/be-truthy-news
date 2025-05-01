71dfd5915b94f23b4f13554b6d389b7a50389954
MM <mm@Mac.mynet>
2025-05-01 19:47:15 +0100

    updating listen file

--- listen.js
+++ listen.js
@@ -2,5 +2,5 @@ const app = require("./api");
 
 const { PORT = 9090 } = process.env;
 app.listen(PORT, () => {
-  console.log(`Server is listening on port ${port}...`);
+  console.log(`Server is listening on port ${PORT}...`);
 });
