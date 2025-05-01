422adb430b78e48b8408eb43db859e6c4c5f019e
MM <mm@Mac.mynet>
2025-05-01 15:36:42 +0100

    build checkExists utility

--- db/seeds/utils.js
+++ db/seeds/utils.js
@@ -1,4 +1,5 @@
 const db = require("../../db/connection");
+const format = require("pg-format");
 
 exports.convertTimestampToDate = ({ created_at, ...otherProperties }) => {
   if (!created_at) return { ...otherProperties };
@@ -17,4 +18,14 @@ exports.createReference = (articlesData) => {
   });
 
   return result;
+};
+
+exports.checkExists = (table, column, value) => {
+  const queryStr = format("SELECT * FROM %I WHERE %I = $1", table, column);
+  return db.query(queryStr, [value]).then((result) => {
+    if (result.rows.length === 0) {
+      return Promise.reject({ status: 404, msg: "Not Found" });
+    }
+    return true;
+  });
 };
