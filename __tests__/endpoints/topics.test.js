5ab70b0876c41f582c740db2df830890c8f76ff5
7doublem <mustafamoallim@gmail.com>
2025-06-19 18:50:28 +0100

    add tests for postNewTopic

--- __tests__/endpoints/topics.test.js
+++ __tests__/endpoints/topics.test.js
@@ -42,3 +42,60 @@ describe("GET /api/topics", () => {
       });
   });
 });
+
+describe("POST /api/topics", () => {
+  // happy path
+  test("201: Responds with newly posted topic", () => {
+    return request(app)
+      .post("/api/topics")
+      .send({
+        slug: "test-topic",
+        description: "jest tests for testing topics",
+        img_url: "https://example.com/test-topic.jpg",
+      })
+      .expect(201)
+      .then(({ body: { topic } }) => {
+        expect(topic).toMatchObject({
+          slug: "test-topic",
+          description: "jest tests for testing topics",
+          img_url: "https://example.com/test-topic.jpg",
+        });
+      });
+  });
+  //sad path
+  test("400: Responds with an error message 400: Bad Request for a request body that is missing one of the required fields", () => {
+    return request(app)
+      .post("/api/topics")
+      .send({})
+      .expect(400)
+      .then(({ body }) => {
+        expect(body.msg).toBe("Missing one or more required fields");
+      });
+  });
+  test("400: Responds with an error message 400: Bad Request for request body that has one or more invalid data types", () => {
+    return request(app)
+      .post("/api/topics")
+      .send({
+        slug: 600,
+        description: "jest tests for testing topics",
+        img_url: "https://example.com/test-topic.jpg",
+      })
+      .expect(400)
+      .then(({ body }) => {
+        expect(body.msg).toBe("Invalid data type for one or more fields");
+      });
+  });
+  test("400: Responds with an error message 400: Bad Request for request body that has a duplicate slug", () => {
+    return request(app)
+      .post("/api/topics")
+      .send({
+        slug: "mitch",
+        description: "jest tests for testing topics",
+        img_url: "https://example.com/test-topic.jpg",
+      })
+      .expect(400)
+      .then(({ body }) => {
+        expect(body.msg).toBe("Topic already exists");
+      });
+  });
+});
